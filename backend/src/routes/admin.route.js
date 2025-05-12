0f8d67c219f197355dde35e9cde5dacaf4bbf5d5
rafli4514 <muhammadrafli.tkn@gmail.com>
2025-05-13 04:15:24 +0700

    feat: add route and controller album

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -4,11 +4,12 @@ import {
   deleteSong,
   createAlbum,
   deleteAlbum,
+  checkAdmin,
 } from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 const router = Router();
 
-router.use(protectRoute, requitreAdmin);
+router.use(protectRoute, requireAdmin);
 
 router.get("/check", checkAdmin);
 
